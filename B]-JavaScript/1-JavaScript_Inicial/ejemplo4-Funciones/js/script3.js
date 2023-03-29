b83863b939f2f9f2994b4a625248017b072e8b7f
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-03-29 15:03:29 +0200

    Add files via upload

--- B]-JavaScript/1-JavaScript_Inicial/ejemplo4-Funciones/js/script3.js
+++ B]-JavaScript/1-JavaScript_Inicial/ejemplo4-Funciones/js/script3.js
@@ -15,7 +15,9 @@ A diferencias de otros objetos, digamos este en concreto es estatico.
 */
 function funcionesMath() {
     var numero = 8;
-    document.getElementById('resultado').innerHTML=
+   
+    
+    document.getElementById('resultado').innerHTML=//document.getElementById(apunta al lugar al que hace referencia).PropiedadGeneral
     ' logaritmo: ' + Math.log(1000) + '<br>'+
     'Exponencial 8: ' + Math.exp(numero)+ '<br>'+
     'Raiz Cuadrada: ' + Math.sqrt(9)+ '<br>'+
@@ -31,5 +33,40 @@ function funcionesMath() {
     'Coseno Pi: '  + Math.cos(Math.PI)+ '<br>'+
     'Cambio de nº a String. ' + numero.toString();
 }
+/**
+ * Desde la llamada a una varible, metodos asociados al string, que nos permiten
+ * manipular estas cadenas de manera directa.
+ * Las cadenas de texto, una ventaja que ofrece almacenamiento de informacion (datos)
+ * 
+ */
+function funcionesString(){
+  var texto = 'JavaScript estructuras de cadena de texto';
+  document.getElementById('resultado').innerHTML=//
+  'Minusculas: ' + texto.toLocaleLowerCase()+ '<br>'+
+  'Mayusculas: ' + texto.toLocaleUpperCase()+ '<br>'+
+  'Localizacion de caracter @: ' + texto.indexOf('@') + '<br>'+
+  'Longitud de la cadena de texto: ' + texto.length + '<br>'+
+  'Valor del unicode: ' + texto.charCodeAt(texto.indexOf('J'));
+}
+
+/*
+Objeto Date, integrado en el almacenamiento de fechas y horas,...etc y nos proporciona
+igualmente metodos para manejarlos.
+importante:
+Hemos de asignar a la variable el objeto, como?
+fecha = new Date
+Automaticamente fecha, puede acceder a todos los metodos del objeto (fecha)
+
+*/
+function  funcionesDate() {
+  var fecha = new Date();
+  document.getElementById('resultado').innerHTML=
+  '*fecha: ' + fecha.toLocaleDateString()+ '<br>'+
+  'hora:'+ fecha.toLocaleTimeString()+ '<br>'+
+  'milisengundo: ' + fecha.getMilliseconds() + '<br>'+
+  'mes: ' + fecha.getMonth()+ '<br>'+// asociado a las posiciones de los elementos????
+  '*Dia: ' + fecha.getDay();
+}
+
 
 
