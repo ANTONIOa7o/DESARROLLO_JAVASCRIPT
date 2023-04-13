b9c794e6f66ad7eccb4f32c894224a36616c6325
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-04-13 10:21:36 +0200

    Delete 1-JavaScript_Inicial directory

--- 1-JavaScript_Inicial/ejemplo4-Funciones/js/script7.js
+++
@@ -1,9 +0,0 @@
-function abrir() {
-    var hora = new Date()
-    document.getElementById(`resultado`).innerHTML=
-    hora.getHours() + ':' + hora.getMinutes() + ':' + hora.getMinutes();
-}
-
-function cerrar() {
-    document.getElementById(`resultado`).innerHTML=''
-}
